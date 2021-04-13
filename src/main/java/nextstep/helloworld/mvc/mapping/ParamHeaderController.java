fc5a2591cbba628bfc3e318a9ac38dac0285402d
Eugine Park <pkeugine@gmail.com>
2021-04-14 01:49:39 +0900

    fix: ParamHeaderController 테스트 통과

--- src/main/java/nextstep/helloworld/mvc/mapping/ParamHeaderController.java
+++ src/main/java/nextstep/helloworld/mvc/mapping/ParamHeaderController.java
@@ -9,14 +9,17 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/param-header")
 public class ParamHeaderController {
 
+    @GetMapping(path = "/message")
     public ResponseEntity<String> message() {
         return ResponseEntity.ok().body("message");
     }
 
+    @GetMapping(path = "/message", params = "name=hello")
     public ResponseEntity<String> messageForParam() {
         return ResponseEntity.ok().body("hello");
     }
 
+    @GetMapping(path = "/message", headers = "HEADER=hi")
     public ResponseEntity<String> messageForHeader() {
         return ResponseEntity.ok().body("hi");
     }
