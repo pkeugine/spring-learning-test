8521d38ba391ef02bbf6bb04f432f1d650fcff1c
Eugine Park <pkeugine@gmail.com>
2021-04-13 22:49:16 +0900

    fix: HttpMethodController 테스트 통과

--- src/main/java/nextstep/helloworld/mvc/mapping/HttpMethodController.java
+++ src/main/java/nextstep/helloworld/mvc/mapping/HttpMethodController.java
@@ -1,21 +1,27 @@
 package nextstep.helloworld.mvc.mapping;
 
-import nextstep.helloworld.mvc.domain.User;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
-
 import java.net.URI;
 import java.util.Arrays;
 import java.util.List;
+import nextstep.helloworld.mvc.domain.User;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/http-method")
 public class HttpMethodController {
 
+    @PostMapping("/users")
     public ResponseEntity createUser(@RequestBody User user) {
         Long id = 1L;
         return ResponseEntity.created(URI.create("/users/" + id)).build();
     }
 
+    @GetMapping("/users")
     public ResponseEntity<List<User>> showUser() {
         List<User> users = Arrays.asList(
                 new User("이름", "email"),
