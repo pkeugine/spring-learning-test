14bb6f990ba9b3aaa8da1ae894df7e5e680f97d1
Eugine Park <pkeugine@gmail.com>
2021-04-13 23:55:01 +0900

    fix: MediaTypeController 테스트 통과

--- src/main/java/nextstep/helloworld/mvc/mapping/MediaTypeController.java
+++ src/main/java/nextstep/helloworld/mvc/mapping/MediaTypeController.java
@@ -1,23 +1,30 @@
 package nextstep.helloworld.mvc.mapping;
 
-import nextstep.helloworld.mvc.domain.User;
-import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
+import static org.springframework.http.MediaType.TEXT_HTML_VALUE;
 
 import java.net.URI;
 import java.util.Arrays;
 import java.util.List;
+import nextstep.helloworld.mvc.domain.User;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/media-type")
 public class MediaTypeController {
 
+    @PostMapping(path = "/users", consumes = "application/json")
     public ResponseEntity createUser(@RequestBody User user) {
         Long id = 1L;
         return ResponseEntity.created(URI.create("/users/" + id)).build();
     }
 
+    @GetMapping(path = "/users", produces = APPLICATION_JSON_VALUE)
     public ResponseEntity<List<User>> showUser() {
         List<User> users = Arrays.asList(
                 new User("이름", "email"),
@@ -26,6 +33,7 @@ public class MediaTypeController {
         return ResponseEntity.ok().body(users);
     }
 
+    @GetMapping(path = "/users", produces = TEXT_HTML_VALUE)
     public String userPage() {
         return "user page";
     }
